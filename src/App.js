65c9933db8aabde8967ab2cbbe2d73246f5b18cb
Johanna Keïta <keitajohanna@gmail.com>
2023-08-10 19:35:05 +0200

    Projet React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Ma Première app React</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mon app React
         </a>
       </header>
     </div>
